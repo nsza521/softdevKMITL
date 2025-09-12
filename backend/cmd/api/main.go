e2943e016c6b457a0edb3a41a5a9b66068954188
SrmxKub <66010853@kmitl.ac.th>
2025-09-12 20:20:11 +0700

    seeding customers and restaurants

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -77,6 +77,13 @@ func initMySQL() (*gorm.DB, error) {
 	); err != nil {
 		return nil, fmt.Errorf("error migrating database: %v", err)
 	}
+	log.Println("Database connected and migrated successfully")
+
+	err = InitAllSeedData(db)
+	if err != nil {
+		return nil, fmt.Errorf("error seeding database: %v", err)
+	}
+	log.Println("Database seeding completed successfully")
 
 	return db, nil
 }
