3ef8461ea6d81fff661c81d7e2549b3c8f8367ac
SrmxKub <66010853@kmitl.ac.th>
2025-09-21 15:12:26 +0700

    edit docker-compose env config for dev

--- backend/internal/restaurant/delivery/http/routers.go
+++ backend/internal/restaurant/delivery/http/routers.go
@@ -9,7 +9,7 @@ import (
 
 func MapRestaurantRoutes(restaurantGroup *gin.RouterGroup, restaurantHandler interfaces.RestaurantHandler) {
 	restaurantGroup.POST("/register", restaurantHandler.Register())
-	restaurantGroup.GET("/all", middleware.AuthMiddleware(), restaurantHandler.GetAll())
+	restaurantGroup.GET("/all", restaurantHandler.GetAll())
 	restaurantGroup.POST("/upload_pic", middleware.AuthMiddleware(), restaurantHandler.UploadProfilePicture())
 	restaurantGroup.PATCH("/status", middleware.AuthMiddleware(), restaurantHandler.ChangeStatus()) // No auth middleware here, should be added later
 }
