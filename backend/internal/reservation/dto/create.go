1a2c74f294d2d52dddcafb481079398892513ee8
SrmxKub <66010853@kmitl.ac.th>
2025-10-10 18:44:23 +0700

    change method in customer module and add create random reservation api

--- backend/internal/reservation/dto/create.go
+++ backend/internal/reservation/dto/create.go
@@ -7,7 +7,7 @@ import(
 type CreateTableReservationRequest struct {
 	TableTimeslotID     uuid.UUID 	`json:"table_timeslot_id"`
 	// CustomerID 			uuid.UUID 	`json:"customer_id" binding:"required"`
-	Random           	bool      	`json:"random" binding:"required"` // e.g., true for "random", false for "not random"
+	Random           	bool      	`json:"random"` // e.g., true for "random", false for "not random"
 	// Status          	string      `json:"status" binding:"omitempty"` // e.g., "pending", "confirmed", "canceled"
 	Members 	  		[]Username	`json:"members" binding:"required"`
 }
