cdddff32ee047c6b4ed7c4ba69b62933ecde9e1a
sgissweet <nitkamon.67908@gmail.com>
2025-10-10 11:34:29 +0700

    edit footer nav style

--- frontend/app/reserveFillUsr/page.tsx
+++ frontend/app/reserveFillUsr/page.tsx
@@ -1,6 +1,11 @@
 "use client";
 import styles from "./reserveFillUsr.module.css";
+import { useSearchParams } from "next/navigation";
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+type UUID = string;
+
 
 export default function ReserveFillUsrPage() {
     
