3f02ad8cfd9a6a51bcaf1639c674ab7a1baba1ee
SrmxKub <66010853@kmitl.ac.th>
2025-10-12 02:57:44 +0700

    add CRUD of table and timeslot

--- backend/internal/table/delivery/http/routers.go
+++ backend/internal/table/delivery/http/routers.go
@@ -10,18 +10,23 @@ import (
 func MapTableRoutes(tableGroup *gin.RouterGroup, tableHandler interfaces.TableHandler) {
 	// Table routes
 	tableGroup.POST("/create", tableHandler.CreateTable())
+	tableGroup.GET("/all", tableHandler.GetAllTables())
+	tableGroup.GET("/:table_id", tableHandler.GetTableByID())
+	tableGroup.PUT("/:table_id", tableHandler.EditTableDetails())
+	tableGroup.DELETE("/:table_id", tableHandler.DeleteTable())
 
 	// Timeslot routes
 	tableGroup.POST("/timeslot/create", tableHandler.CreateTimeslot())
 	tableGroup.GET("/timeslot/all", tableHandler.GetAllTimeslots())
+	tableGroup.GET("/timeslot/:timeslot_id/details", tableHandler.GetTimeslotByID())
+	tableGroup.PUT("/timeslot/:timeslot_id", tableHandler.EditTimeslotDetails())
+	tableGroup.DELETE("/timeslot/:timeslot_id", tableHandler.DeleteTimeslot())
+
+	// TableTimeslot routes
+	tableGroup.GET("/timeslot/:timeslot_id", tableHandler.GetTableTimeslotByTimeslotID()) // will be change to GET /table_timeslot/:timeslot_id/all
 
 
 	tableGroup.Use(middleware.AuthMiddleware())
-	// Table routes
-	tableGroup.GET("/all", tableHandler.GetAllTables())
-	
-	// Timeslot routes
-	tableGroup.GET("/timeslot/:timeslot_id", tableHandler.GetTableTimeslotByTimeslotID())
 
 	// TableTimeslot routes
 	tableGroup.GET("/table_timeslot/now", tableHandler.GetNowTableTimeslots())
