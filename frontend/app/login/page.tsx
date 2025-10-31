92fc538de37217e592847014a30e1965cffaea45
nsza <128568016+nsza521@users.noreply.github.com>
2025-10-31 20:17:19 +0700

    this is the final nam branch
    this is the final nam branch

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -22,7 +22,7 @@ export default function LoginPage() {
     setLoading(true);
 
     try {
-      const res = await fetch("http://localhost:8080/customer/login", {
+      const res = await fetch("http://localhost:8080/restaurant/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password }),
