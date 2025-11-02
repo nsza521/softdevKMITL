784997b68c1dede4a1579a37f89841ef4512fe2d
SrmxKub <66010853@kmitl.ac.th>
2025-11-03 01:26:51 +0700

    edit status

--- backend/internal/reservation/usecase/usecase.go
+++ backend/internal/reservation/usecase/usecase.go
@@ -742,7 +742,8 @@ func (u *TableReservationUsecase) CancelTableReservationMember(reservationID uui
 	// If all members have paid → update reservation status to "paid"
 	allPaid := true
 	for _, member := range members {
-		if member.Status != "paid" && member.Status != "paid_pending" {
+		// if member.Status != "paid" && member.Status != "paid_pending" {
+		if member.Status != "paid" {
 			allPaid = false
 			break
 		}
