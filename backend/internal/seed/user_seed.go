5fcbda52c698f26994cfef4474e90d809441ea64
SrmxKub <66010853@kmitl.ac.th>
2025-11-03 02:51:22 +0700

    upload restaurant image when seeding

--- backend/internal/seed/user_seed.go
+++ backend/internal/seed/user_seed.go
@@ -2,11 +2,44 @@ package seed
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
+	"mime/multipart"
+	"net/textproto"
 	"gorm.io/gorm"
+	"github.com/minio/minio-go/v7"
 
 	"backend/internal/db_model"
 	"backend/internal/utils"
 )
+
+func uploadSampleRestaurantImage(minioClient *minio.Client, filename string) (string, error) {
+	basePath, _ := os.Getwd() // current working directory ของ process
+	filePath := filepath.Join(basePath, "internal", "assets", "images", "restaurants", filename)
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		return "", fmt.Errorf("open file error: %v", err)
+	}
+	defer file.Close()
+
+	fileInfo, _ := file.Stat()
+	fileHeader := &multipart.FileHeader{
+		Filename: filepath.Base(filePath),
+		Size:     fileInfo.Size(),
+		Header:   make(textproto.MIMEHeader),
+	}
+	fileHeader.Header.Set("Content-Type", "image/png")
+
+	// upload to MinIO
+	objectName := fmt.Sprintf("restaurants/%s", fileHeader.Filename)
+	url, err := utils.UploadImage(file, fileHeader, "restaurant-pictures", objectName, minioClient)
+	if err != nil {
+		return "", err
+	}
+	return url, nil
+}
+
 
 func seedCustomers(db *gorm.DB) error {
 
@@ -42,8 +75,8 @@ func seedCustomers(db *gorm.DB) error {
 	return nil
 }
 
-func seedRestaurants(db *gorm.DB) error {
-	for i := 1; i <= 10; i++ {
+func seedRestaurants(db *gorm.DB, minioClient *minio.Client) error {
+	for i := 1; i <= 2; i++ {
 		username := fmt.Sprintf("restaurant%02d", i)
 		email := fmt.Sprintf("restaurant%02d@example.com", i)
 		name := fmt.Sprintf("ร้านข้าว%02d", i)
@@ -61,6 +94,13 @@ func seedRestaurants(db *gorm.DB) error {
 			return err
 		}
 
+		// ---- Upload image to MinIO ----
+		filename := fmt.Sprintf("restaurant%02d.png", i)
+		imgURL, err := uploadSampleRestaurantImage(minioClient, filename)
+		if err != nil {
+			return fmt.Errorf("failed to upload image for %s: %v", name, err)
+		}
+
 		// Create new restaurant
 		restaurant := models.Restaurant{
 			Username: username,
@@ -68,6 +108,7 @@ func seedRestaurants(db *gorm.DB) error {
 			Email:    email,
 			Password: hashedPassword,
 			WalletBalance: 200, // default wallet balance
+			ProfilePic: &imgURL,
 		}
 		if err := db.Create(&restaurant).Error; err != nil {
 			return err
